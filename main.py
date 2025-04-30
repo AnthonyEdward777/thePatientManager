c2211ada7f0ffe927885a3ce7b74c0f18bc4038c
Anthony Edward <Anthony_ehab@hotmail.com>
2025-04-30 17:08:41 +0300

    added the main classes

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Patient management system")
+
+class patients:
+    def __init__(self, name, age, NID):
+        self.name = name
+        self.age = age
+        self.NID = NID
+        
+class doctors:
+    def __init__(self, name, age, NID):
+        self.name = name
+        self.age = age
+        self.NID = NID
+
+class appointments:
+    def __init__(self, patient, doctor, date, room):
+        self.patient = patient
+        self.doctor = doctor
+        self.date = date
+        self.room = room
