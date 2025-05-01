975b2a75dff0baa4b82c239e3876faba0b689c20
Anthony Edward <Anthony_ehab@hotmail.com>
2025-05-01 17:36:43 +0300

    added some attributes to the classes

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ conn = sqlite3.connect('clinic.db')
 c = conn.cursor()
 
 class patients:
-    def __init__(self, name, age, NID):
+    def __init__(self, name, age, NID, phone, address):
         self.name = name
         self.age = age
         self.NID = NID
-        
+        self.phone = phone
+        self.address = address
+
 class doctors:
-    def __init__(self, name, age, NID):
+    def __init__(self, name, age, NID, pay, specialization):
         self.name = name
         self.age = age
         self.NID = NID
+        self.pay = pay
+        self.specialization = specialization
 
 class appointments:
     def __init__(self, patient, doctor, date, room):
