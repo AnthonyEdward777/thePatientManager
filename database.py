1172d1cff78a377535353151031b021c291250a9
Anthony Edward <Anthony_ehab@hotmail.com>
2025-05-13 18:42:14 +0300

    defined the 2 remaining objects

--- database.py
+++ database.py
@@ -75,3 +75,82 @@ class doctors:
         doctor = c.fetchone()
         conn.close()
         return doctor[0] > 0
+    
+class patients:
+    def __init__(self, name, age, NID):
+        self.name = name
+        self.age = age
+        self.NID = NID
+    
+    def fetchAllPatients(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM patients")
+        patients = c.fetchall()
+        conn.close()
+        return patients
+    
+    def addPatient(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO patients (name, age) VALUES (?, ?)", (self.name, self.NID,self.age))
+        conn.commit()
+        conn.close()   
+    
+    def deletePatient(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("DELETE FROM patients WHERE NID=?", (self.NID,))
+        conn.commit()
+        conn.close()
+    
+    def updatePatient(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("UPDATE patients SET name=?, age=? WHERE NID=?", (self.name, self.age, self.NID))
+        conn.commit()
+        conn.close()
+        
+    def patExists(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("SELECT COUNT(*) FROM patients WHERE NID=?", (self.NID,))
+        patient = c.fetchone()
+        conn.close()
+        return patient[0] > 0
+    
+class appointments:
+    def __init__(self, doctorName, patientName, date, ID):
+        self.doctorName = doctorName
+        self.patientName = patientName
+        self.date = date
+        self.ID = ID
+    
+    def fetchAllAppointments(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM appointments")
+        appointments = c.fetchall()
+        conn.close()
+        return appointments
+    
+    def addAppointment(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO appointments (doctorName, patientName, date) VALUES (?, ?, ?)", (self.doctorName, self.patientName, self.date))
+        conn.commit()
+        conn.close()   
+    
+    def deleteAppointment(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("DELETE FROM appointments WHERE ID=?", (self.ID,))
+        conn.commit()
+        conn.close()
+    
+    def updateAppointment(self):
+        conn = sql.connect('clinic.db')
+        c = conn.cursor()
+        c.execute("UPDATE appointments SET doctorName=?, patientName=?, date=? WHERE ID=?", (self.doctorName, self.patientName, self.date, self.ID))
+        conn.commit()
+        conn.close()
--- gui.py
+++ gui.py
@@ -1,19 +1,7 @@
 import customtkinter as ctk
+from tkinter import messagebox
+from tkinter import ttk
+import tkinter as tk
+from tkinter import *
+import database
 
-class App(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.title("The Patient Manager")
-        self.geometry("500x400")
-        
-        self.tabview = ctk.CTkTabview(self)
-        self.tabview.pack(pady=20, padx=20, expand=True, fill="both")
-
-        self.doctorTab = self.tabview.add("Doctors")
-        self.addDoctorTab_content()
-
-        self.patientsTab = self.tabview.add("Patients")
-        self.addPatientTab_content()
-        
-        self.appointmentsTab = self.tabview.add("Appointments")
-        self.addAppointmentTab_content()
