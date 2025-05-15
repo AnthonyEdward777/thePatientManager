ae21880da72a3379b547a54fe68520790fc60e52
kil4nii <kilanymohamed56@gmail.com>
2025-05-15 02:03:09 +0200

    hiii

--- gui.py
+++ gui.py
@@ -1,7 +1,18 @@
-import customtkinter as ctk
-from tkinter import messagebox
-from tkinter import ttk
-import tkinter as tk
+import customtkinter
 from tkinter import *
+from tkinter import messagebox
+from datetime import date
 import database
 
+app = customtkinter.CTk()
+app.title('Clinic Management System')
+app.geometry('900x350')
+app.config(bg='#0A0B0C')
+app.iconbitmap('clinic_icon.ico')  # You would need a clinic-themed icon
+app.resizable(False, False)
+
+font1 = ('Arial', 20, 'bold')
+font2 = ('Arial', 24, 'bold')
+
+
+app.mainloop()
